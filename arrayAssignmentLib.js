44c82f8de1bc7644ff80147ca59eec1efbc89b15
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2018-10-17 12:03:37 +0530

    Added function to map length of every element of an array.
    This function creates a new array that consists of lengths of each
    element and returns the array.

---
+++ arrayAssignmentLib.js
@@ -0,0 +1,55 @@
+exports.findOddNumbers = function(listOfNumbers) {
+  let result = [];
+  for(number of listOfNumbers) {
+    let isOdd = (number % 2 == 1);
+    if(isOdd) {
+      result = result.concat(number);
+    }
+  }
+  return result;
+}
+
+exports.findEvenNumbers = function(listOfNumbers) {
+  let result = [];
+  for(number of listOfNumbers) {
+    let isEven = (number % 2 == 0);
+    if(isEven) {
+      result = result.concat(number);
+    }
+  }
+  return result;
+}
+
+exports.sumOfNumbers = function(listOfNumbers) {
+  let sum = 0;
+  for(number of listOfNumbers) {
+    sum = sum + number;
+  }
+  return sum;
+}
+
+exports.selectEverySecondElement = function(listOfNumbers) {
+  let result = [];
+  for(let index = 0; index < listOfNumbers.length; index = index + 2) {
+    result.push(listOfNumbers[index]);
+  }
+  return result;
+}
+
+exports.greatestNumberFromList = function(listOfNumbers) {
+  let result = 0;
+  for(number of listOfNumbers) {
+    if(result<number){
+      result = number;
+    }
+  }
+  return result;
+}
+
+exports.mapElementsLength = function(listOfElements) {
+  let result = [];
+  for(element of listOfElements) {
+    result.push(element.length); 
+  }
+  return result;
+}
